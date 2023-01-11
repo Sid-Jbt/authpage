f6b03605315cefb252a2d3cda11c1c04076fdc47
Mridul <mridul.rathore@jarvisbitz.com>
2023-01-11 12:01:50 +0530

    Added Components

--- src/Components/DashboardLayout/index.js
+++ src/Components/DashboardLayout/index.js
@@ -8,27 +8,27 @@ const DashboardLayout = ({ bgColor, children, ...rest }) => {
   const customization = useSelector((state) => state.customization);
   return (
     <Box
-      sx={({ breakpoints, transitions, functions: { pxToRem } }) => ({
-        p: 3,
-        [breakpoints.up('xl')]: {
-          marginLeft: !customization.opened ? pxToRem(120) : pxToRem(274),
-          transition: transitions.create(['margin-left', 'margin-right'], {
-            easing: transitions.easing.easeInOut,
-            duration: transitions.duration.standard
-          })
-        }
-      })}
+      bgColor={background || 'info'}
+      height="300px"
+      width="100vw"
+      position="absolute"
+      top={0}
+      left={0}
+      {...rest}
     >
-      <DashboardNavbar />
       <Box
-        bgColor={background || 'info'}
-        height="300px"
-        width="100vw"
-        position="absolute"
-        top={0}
-        left={0}
-        {...rest}
+        sx={({ breakpoints, transitions, functions: { pxToRem } }) => ({
+          p: 3,
+          [breakpoints.up('xl')]: {
+            marginLeft: customization.miniSidenav ? pxToRem(120) : pxToRem(274),
+            transition: transitions.create(['margin-left', 'margin-right'], {
+              easing: transitions.easing.easeInOut,
+              duration: transitions.duration.standard
+            })
+          }
+        })}
       >
+        <DashboardNavbar />
         <Outlet />
       </Box>
     </Box>
