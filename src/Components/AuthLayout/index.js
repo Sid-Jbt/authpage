3ab02f91779560dfca5aeb74a4d7d779fc2420ce
hetali-jarvisbitz <hetali.shah@jarvisbitz.com>
2023-01-11 11:26:00 +0530

    Done: Login page UI #2

--- src/Components/AuthLayout/index.js
+++ src/Components/AuthLayout/index.js
@@ -4,80 +4,82 @@ import Typography from 'Elements/Typography';
 import { Outlet } from 'react-router';
 import { Grid } from '@mui/material';
 
-const AuthLayout = ({ color, background, illustration }) => (
-  <>
-    <Box
-      width="100vw"
-      height="100%"
-      minHeight="100vh"
-      bgColor={background}
-      sx={{ overflowX: 'hidden' }}
-    >
-      <Box mt={1}>Header</Box>
-      <Grid container>
-        <Grid item xs={11} sm={8} md={6} lg={4} xl={3} sx={{ mx: 'auto' }}>
-          <Box display="flex" flexDirection="column" justifyContent="center" height="100vh">
-            <Box p={3}>
-              <Outlet />
+const AuthLayout = ({ color }) => {
+  const bgImage =
+    'https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg';
+  return (
+    <>
+      <Box
+        width="100vw"
+        height="100%"
+        minHeight="100vh"
+        bgColor="white"
+        sx={{ overflowX: 'hidden' }}
+      >
+        <Grid container>
+          <Grid item xs={11} sm={8} md={6} lg={4} xl={3} sx={{ mx: 'auto' }}>
+            <Box mt={1}>Header</Box>
+            <Box display="flex" flexDirection="column" justifyContent="center" height="100vh">
+              <Box p={3}>
+                <Outlet />
+              </Box>
             </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={12} lg={6}>
-          <Box
-            display={{ xs: 'none', lg: 'flex' }}
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            width="calc(100% - 2rem)"
-            height="calc(100vh - 2rem)"
-            position="relative"
-            borderRadius="lg"
-            textAlign="center"
-            m={2}
-            px={13}
-            sx={{ overflow: 'hidden' }}
-          >
+          </Grid>
+          <Grid item xs={12} lg={6}>
             <Box
-              component="img"
-              src={illustration?.image}
-              alt="background"
-              width="100%"
-              position="absolute"
-              top={0}
-              left={0}
-            />
-            <Box
-              bgColor={color}
-              variant="gradient"
-              width="100%"
-              height="100%"
-              position="absolute"
-              topl={0}
-              left={0}
-              opacity={0.7}
-            />
-            <Box position="relative">
-              {illustration?.title && (
+              display={{ xs: 'none', lg: 'flex' }}
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              width="calc(100% - 2rem)"
+              height="calc(100vh - 2rem)"
+              position="relative"
+              borderRadius="lg"
+              textAlign="center"
+              m={2}
+              px={13}
+              sx={{ overflow: 'hidden' }}
+            >
+              <Box
+                component="img"
+                src={bgImage}
+                alt="background"
+                width="100%"
+                position="absolute"
+                top={0}
+                left={0}
+              />
+              <Box
+                bgColor={color}
+                variant="gradient"
+                width="100%"
+                height="100%"
+                position="absolute"
+                topl={0}
+                left={0}
+                opacity={0.7}
+              />
+              <Box position="relative">
                 <Box mt={6} mb={1}>
                   <Typography variant="h4" color="white" fontWeight="bold">
-                    {illustration?.title}
+                    Attention is the new currency
                   </Typography>
                 </Box>
-              )}
-              {illustration?.description && (
+
                 <Box mb={1}>
                   <Typography variant="body2" color="white">
-                    {illustration?.description}
+                    The more effortless the writing looks, the more effort the writer actually put
+                    into the process.
                   </Typography>
                 </Box>
-              )}
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-      Footer
-    </Box>
-  </>
-);
+        Footer
+      </Box>
+    </>
+  );
+};
 
 export default AuthLayout;
--- src/Screens/Auth/Login/index.js
+++ src/Screens/Auth/Login/index.js
@@ -1,14 +1,61 @@
-import { Switch } from '@mui/material';
-import Button from 'Elements/Button';
 import React from 'react';
+import { Switch } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Box from 'Elements/Box';
+import Typography from 'Elements/Typography';
+import Input from 'Elements/Input';
+import Button from 'Elements/Button';
 
 const Login = () => (
-  <div style={{ margin: 10 }}>
-    <Button variant="contained" color="info">
-      Test
-    </Button>
-    <Switch />
-  </div>
+  <Box>
+    <Box mb={1}>
+      <Typography variant="h4" fontWeight="bold">
+        Sign In
+      </Typography>
+    </Box>
+    <Typography variant="body2" fontWeight="regular" color="text" mb={1}>
+      Enter your email and password to sign in
+    </Typography>
+
+    <Box component="form" role="form">
+      <Box mb={2}>
+        <Input type="email" placeholder="Email" size="large" />
+      </Box>
+      <Box mb={2}>
+        <Input type="password" placeholder="Password" size="large" />
+      </Box>
+      <Box display="flex" alignItems="center">
+        <Switch />
+        <Typography
+          variant="button"
+          fontWeight="regular"
+          onClick={() => console.log('hello')}
+          sx={{ cursor: 'pointer', userSelect: 'none' }}
+        >
+          &nbsp;&nbsp;Remember me
+        </Typography>
+      </Box>
+      <Box mt={4} mb={1}>
+        <Button color="info" size="large" fullWidth onClick={() => console.log('onPressSignIn')}>
+          Sign In
+        </Button>
+      </Box>
+      <Box mt={3} textAlign="center">
+        <Typography variant="button" color="text" fontWeight="regular">
+          Don&apos;t have an account?{' '}
+          <Typography
+            component={Link}
+            to="/authentication/sign-up"
+            variant="button"
+            color="info"
+            fontWeight="medium"
+          >
+            Sign up
+          </Typography>
+        </Typography>
+      </Box>
+    </Box>
+  </Box>
 );
 
 export default Login;
