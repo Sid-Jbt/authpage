0fafb37706dd43a16757490d4f83cb4b1d74fb8d
hetali-jarvisbitz <hetali.shah@jarvisbitz.com>
2023-01-11 15:23:03 +0530

    WIP: Formik and yup

--- src/Screens/Auth/Login/index.js
+++ src/Screens/Auth/Login/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Switch } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import Box from 'Elements/Box';
 import Typography from 'Elements/Typography';
 import Input from 'Elements/Input';
@@ -8,7 +10,22 @@ import Button from 'Elements/Button';
 const Login = () => {
   const [rememberMe, setRememberMe] = useState(false);
 
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: yup.object().shape({
+      email: yup.string('Enter your email').email(true).required('Email is required')
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
+  console.log('shfjksgfuisgf', formik.values, formik.errors);
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
   return (
     <Box>
       <Box mb={1}>
@@ -19,11 +36,22 @@ const Login = () => {
       <Typography variant="body2" fontWeight="regular" color="text" mb={1}>
         Enter your email and password to sign in
       </Typography>
-
       <Box component="form" role="form">
-        <Box mb={2}>
-          <Input type="email" placeholder="Email" size="large" />
-        </Box>
+        <form onSubmit={formik.handleSubmit}>
+          <Box mb={2}>
+            <Input
+              type="email"
+              placeholder="Email"
+              size="large"
+              id="email"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.errors.email}
+            />
+          </Box>
+        </form>
         <Box mb={2}>
           <Input type="password" placeholder="Password" size="large" />
         </Box>
