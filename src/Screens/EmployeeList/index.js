7823ceaa315ef7034712b95bc494ab5e4eb25914
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-01-13 18:37:49 +0530

    Remove extra box containt

--- src/Screens/EmployeeList/index.js
+++ src/Screens/EmployeeList/index.js
@@ -9,36 +9,32 @@ const EmployeeList = () => {
   const { columns: prCols, rows: prRows } = employeeListData;
   console.log('employeeListData --> ', employeeListData);
   return (
-    <Box py={3}>
+    <Card
+      mb={3}
+      sx={{
+        py: 4,
+        px: 2,
+        background: ({ palette: { white } }) => white.main,
+        borderRadius: ({ borders: { borderRadius } }) => borderRadius.xl,
+        boxShadow: ({ boxShadows: { md } }) => md
+      }}
+    >
+      <Box display="flex" justifyContent="space-between" alignItems="center" pb={3}>
+        <Typography variant="h6">Employee List</Typography>
+      </Box>
       <Box
-        mb={3}
         sx={{
-          py: 2,
-          px: 2,
-          background: ({ palette: { white } }) => white.main,
-          borderRadius: ({ borders: { borderRadius } }) => borderRadius.xl,
-          boxShadow: ({ boxShadows: { md } }) => md
+          '& .MuiTableRow-root:not(:last-child)': {
+            '& td': {
+              borderBottom: ({ borders: { borderWidth, borderColor } }) =>
+                `${borderWidth[1]} solid ${borderColor}`
+            }
+          }
         }}
       >
-        <Card>
-          <Box display="flex" justifyContent="space-between" alignItems="center" p={3}>
-            <Typography variant="h6">Employee List</Typography>
-          </Box>
-          <Box
-            sx={{
-              '& .MuiTableRow-root:not(:last-child)': {
-                '& td': {
-                  borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                    `${borderWidth[1]} solid ${borderColor}`
-                }
-              }
-            }}
-          >
-            <Table columns={prCols} rows={prRows} />
-          </Box>
-        </Card>
+        <Table columns={prCols} rows={prRows} />
       </Box>
-    </Box>
+    </Card>
   );
 };
 
