8a44e9214879cda78c1f82cf71bb5159d3a73342
hetali-jarvisbitz <hetali.shah@jarvisbitz.com>
2023-01-16 21:07:25 +0530

    WIP: profile menu

--- src/Components/DashboardLayout/Navbar/index.js
+++ src/Components/DashboardLayout/Navbar/index.js
@@ -1,22 +1,27 @@
-import { AppBar, Icon, IconButton, Menu, Toolbar } from '@mui/material';
+import { AppBar, Avatar, Icon, IconButton, Menu, Toolbar } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Box from 'Elements/Box';
 import { MINI_SIDENAV } from 'Redux/actions/ui/actions';
-import { MenuOpenTwoTone, MenuTwoTone, Notifications } from '@mui/icons-material';
+import { MenuOpenTwoTone, MenuTwoTone, Notifications, Person } from '@mui/icons-material';
 import { useState } from 'react';
 import NotificationItem from 'Elements/Item';
 
+import profileImage from 'Assets/Images/bruce-mars.jpg';
 import { navbar, navbarContainer, navbarIconButton, navbarRow } from './styles';
 
 const DashboardNavbar = ({ isMini }) => {
   const customization = useSelector((state) => state.customization);
   const dispatch = useDispatch();
   const [openMenu, setOpenMenu] = useState(false);
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
 
   const handleMiniSidenav = () =>
     dispatch({ type: MINI_SIDENAV, value: !customization.miniSidenav });
+
   const handleMenu = () => setOpenMenu(!openMenu);
+
+  const handleProfileMenu = () => setOpenProfileMenu(!openProfileMenu);
 
   const renderMenu = () => (
     <Menu
@@ -28,7 +33,7 @@ const DashboardNavbar = ({ isMini }) => {
       }}
       open={Boolean(openMenu)}
       onClose={handleMenu}
-      sx={{ mt: 2, top: 77 }}
+      sx={{ mt: 2, top: 30 }}
     >
       <NotificationItem
         // image={<img src={team2} alt="person" />}
@@ -56,6 +61,33 @@ const DashboardNavbar = ({ isMini }) => {
     </Menu>
   );
 
+  const renderProfileMenu = () => (
+    <Menu
+      anchorEl={openProfileMenu}
+      anchorReference={null}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      open={Boolean(openProfileMenu)}
+      onClose={handleProfileMenu}
+      sx={{ mt: 2, top: 30 }}
+    >
+      <NotificationItem image={<Person />} title={['Profile']} onClick={handleProfileMenu} />
+      <NotificationItem
+        color="secondary"
+        image={
+          <Icon fontSize="small" sx={{ color: ({ palette: { white } }) => white.main }}>
+            payment
+          </Icon>
+        }
+        title={['', 'Payment successfully completedasfdsfdsf']}
+        date="2 days"
+        onClick={handleProfileMenu}
+      />
+    </Menu>
+  );
+
   return (
     <AppBar position="sticky" color="inherit" sx={(theme) => navbar(theme)}>
       <Toolbar sx={(theme) => navbarContainer(theme, { position: 'static' })}>
@@ -75,7 +107,21 @@ const DashboardNavbar = ({ isMini }) => {
         <Box sx={(theme) => navbarRow(theme, { isMini })}>
           <Box color="white">
             <IconButton
-              size="small"
+              size="medium"
+              color="white"
+              sx={navbarIconButton}
+              variant="contained"
+              onClick={handleProfileMenu}
+            >
+              <Avatar
+                src={profileImage}
+                alt="profile-image"
+                variant="rounded"
+                sx={{ width: 25, height: 25, borderRadius: 12 }}
+              />
+            </IconButton>
+            <IconButton
+              size="medium"
               color="white"
               sx={navbarIconButton}
               variant="contained"
@@ -84,6 +130,7 @@ const DashboardNavbar = ({ isMini }) => {
               <Notifications />
             </IconButton>
             {renderMenu()}
+            {renderProfileMenu()}
           </Box>
         </Box>
       </Toolbar>
