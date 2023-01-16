686e0611da8dd2354c3660fef0a1729ca0d530fc
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-01-16 10:17:55 +0530

    Update routeConfig

--- src/Routes/routeConfig.js
+++ src/Routes/routeConfig.js
@@ -7,11 +7,18 @@ export const loginPattern = '/login';
 export const getLoginPattern = () => generatePath(loginPattern);
 
 export const dashboardPattern = '/dashboard';
+export const getDashboard = () => generatePath(dashboardPattern);
+
 export const forgotPasswordPattern = '/forgot-password';
-export const resetPasswordPattern = '/reset-password';
 export const getForgotPasswordPattern = () => generatePath(forgotPasswordPattern);
 
+export const resetPasswordPattern = '/reset-password';
+export const getResetPasswordPattern = () => generatePath(resetPasswordPattern);
+
 export const profilePattern = '/profile';
-export const errorPattern = '/error';
-export const employeeListPattern = '/employee-list';
 export const getProfilePattern = () => generatePath(profilePattern);
+
+export const errorPattern = '/error';
+
+export const employeeListPattern = '/employee-list';
+export const getEmployeeListPattern = () => generatePath(employeeListPattern);
