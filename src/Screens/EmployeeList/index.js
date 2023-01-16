62e5a15bb7a499190c2db16596931dc36edfed9e
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-01-16 19:23:01 +0530

    WIP: pagination component

--- src/Screens/EmployeeList/index.js
+++ src/Screens/EmployeeList/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Card, Icon } from '@mui/material';
+import { Card, FormControl, Icon, InputLabel, MenuItem, Select } from '@mui/material';
 import Box from 'Elements/Box';
 import Typography from 'Elements/Typography';
 import Table from 'Elements/Table';
 import Button from 'Elements/Button';
-import { Add, ImportExportRounded } from '@mui/icons-material';
+import { Add, ImportExportRounded, SearchRounded, ClearRounded } from '@mui/icons-material';
+import Input from 'Elements/Input';
+import Paginations from 'Components/Pagination/index';
 import employeeListData from './data/employeeListData';
 
 const EmployeeList = () => {
   const { columns: prCols, rows: prRows } = employeeListData;
+  const [role, setRole] = React.useState('');
 
+  const handleChangeRole = (event) => {
+    setRole(event.target.value);
+  };
+  console.log('Role --> ', role);
   return (
     <Card
       mb={3}
@@ -22,13 +29,13 @@ const EmployeeList = () => {
       <Box display="flex" justifyContent="space-between" alignItems="center" mb={2} p={2} pb={0}>
         <Typography variant="h6">Employee</Typography>
         <Box>
-          <Button color="info" variant="contained" size="small" sx={{ marginRight: '10px' }}>
+          <Button color="info" variant="contained" size="medium" sx={{ marginRight: '10px' }}>
             <Icon sx={{ fontWeight: 'bold', paddingRight: '20px' }}>
               <Add />{' '}
             </Icon>
             Add
           </Button>
-          <Button color="info" variant="contained" size="small">
+          <Button color="info" variant="contained" size="medium">
             <Icon sx={{ fontWeight: 'bold', paddingRight: '20px' }}>
               <ImportExportRounded />{' '}
             </Icon>
@@ -45,7 +52,63 @@ const EmployeeList = () => {
         pb={0}
         pt={0}
       >
-        Filter
+        <Box display="flex" justifyContent="space-between">
+          <Box mr={2}>
+            <Input
+              type="date"
+              placeholder="From Date"
+              size="small"
+              fullWidth
+              id="fromDate"
+              name="fromDate"
+            />
+          </Box>
+          <Box mr={3}>
+            <Input
+              type="date"
+              placeholder="To Date"
+              size="small"
+              fullWidth
+              id="toDate"
+              name="toDate"
+            />
+          </Box>
+          <Box sx={{ minWidth: 130 }} mr={5}>
+            <FormControl fullWidth>
+              <InputLabel id="select-role-label" size="large" pb={3}>
+                Role
+              </InputLabel>
+              <Select
+                labelId="select-role-label"
+                id="selectRole"
+                value={role}
+                label="Select Role"
+                displayEmpty
+                onChange={handleChangeRole}
+                sx={{ borderRadius: '8px' }}
+              >
+                <MenuItem value={10}>Super Admin</MenuItem>
+                <MenuItem value={20}>Admin</MenuItem>
+                <MenuItem value={30}>Developer</MenuItem>
+                <MenuItem value={40}>HR</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box>
+            <Button color="info" variant="contained" size="medium" sx={{ marginRight: '10px' }}>
+              <Icon sx={{ fontWeight: 'bold', paddingRight: '20px' }}>
+                <SearchRounded />{' '}
+              </Icon>
+              Search
+            </Button>
+            <Button color="info" variant="contained" size="medium">
+              <Icon sx={{ fontWeight: 'bold', paddingRight: '20px' }}>
+                <ClearRounded />{' '}
+              </Icon>
+              Clear
+            </Button>
+          </Box>
+        </Box>
       </Box>
       <Card
         sx={{
@@ -66,6 +129,7 @@ const EmployeeList = () => {
         >
           <Table columns={prCols} rows={prRows} />
         </Box>
+        <Paginations />
       </Card>
     </Card>
   );
