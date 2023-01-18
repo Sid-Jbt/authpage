6cf1dd7b7cc0b5040f967dda1724e7a781021613
sonika-jarvisbitz <sonika.virani@jarvisbitz.com>
2023-01-18 17:09:45 +0530

    Done: Create modal for expense fields

--- src/Screens/Expense/index.js
+++ src/Screens/Expense/index.js
@@ -1,6 +1,6 @@
-import { Card, Grid, Icon } from '@mui/material';
+import { Card, Grid, Icon, Menu } from '@mui/material';
 import { Add, ClearRounded, SearchRounded } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import Box from 'Elements/Box';
 import Typography from 'Elements/Typography';
 import Button from 'Elements/Button';
@@ -10,6 +10,94 @@ import Input from '../../Elements/Input';
 
 const Expense = () => {
   const { columns: prCols, rows: prRows } = expenseListData;
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenu = () => setOpenMenu(!openMenu);
+
+  const renderMenu = () => (
+    <Menu
+      anchorEl={openMenu}
+      anchorReference={null}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      open={Boolean(openMenu)}
+      onClose={handleMenu}
+      sx={{ mt: 1, top: 160, left: -70 }}
+    >
+      <Box mb={0.5}>
+        <Input
+          placeholder="Item name"
+          label="ITEM NAME"
+          size="large"
+          fullWidth
+          id="item_name"
+          name="item_name"
+        />
+      </Box>
+      <Box mb={0.5}>
+        <Input
+          placeholder="Purchase from"
+          label="PURCHASE FROM"
+          size="large"
+          fullWidth
+          id="purchase_from"
+          name="purchase_from"
+        />
+      </Box>
+      <Box mb={0.5}>
+        <Input
+          placeholder="Purchase date"
+          label="PURCHASE DATE"
+          size="large"
+          fullWidth
+          id="purchase_date"
+          name="purchase_date"
+        />
+      </Box>
+      <Box mb={0.5}>
+        <Input
+          placeholder="Amount"
+          label="AMOUNT"
+          size="large"
+          fullWidth
+          id="amount"
+          name="amount"
+        />
+      </Box>
+      <Box mb={0.5}>
+        <Input
+          type="file"
+          placeholder="Password"
+          label="SELECT DOCUMENT"
+          size="large"
+          fullWidth
+          id="select_document"
+          name="select_document"
+        />
+      </Box>
+      <Grid item sm={12} md={4} lg={6}>
+        <Button
+          color="info"
+          variant="contained"
+          size="small"
+          sx={{ marginRight: '10px' }}
+          onClick={handleMenu}
+        >
+          <Icon sx={{ mr: '2px' }}>
+            <Add />
+          </Icon>
+          Add
+        </Button>
+        <Button color="error" variant="contained" size="small" onClick={handleMenu}>
+          <Icon sx={{ mr: '2px' }}>
+            <ClearRounded />
+          </Icon>
+          Clear
+        </Button>
+      </Grid>
+    </Menu>
+  );
 
   return (
     <Card
@@ -29,6 +117,7 @@ const Expense = () => {
               variant="contained"
               size="small"
               sx={{ marginRight: '10px', marginLeft: '40px' }}
+              onClick={handleMenu}
             >
               <Icon sx={{ mr: '2px' }}>
                 <Add />
@@ -63,6 +152,7 @@ const Expense = () => {
         </Grid>
       </Grid>
       <Table columns={prCols} rows={prRows} />
+      {renderMenu()}
     </Card>
   );
 };
