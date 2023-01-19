1b22f54836e5fa1646c673b68aa102e6563464da
bhavy-jarvisbitz <bhavy.kothari@jarvisbitz.com>
2023-01-19 11:13:59 +0530

    Error page updated

--- src/Screens/Error404/index.js
+++ src/Screens/Error404/index.js
@@ -5,34 +5,34 @@ import Button from 'Elements/Button';
 import { Card, Grid } from '@mui/material';
 import { getDashboardPattern } from 'Routes/routeConfig';
 
-// const styles = (theme) => ({
-//   root: {
-//     backgroundColor: 'blue',
-//     [theme.breakpoints.up('md')]: {
-//       backgroundColor: 'red'
-//     }
-//   }
-// });
-
 const Error404 = () => (
-  <Grid sx={{ mt: 13 }} xl={12} lg={12} sm={12}>
-    <Card>
-      <Box sx={{ p: 10 }} alignSelf="center">
-        <Typography variant="h1">404</Typography>
-        <Typography variant="h3">ERROR PAGE</Typography>
-        <Typography>Uh Oh!! you're lost.</Typography>
-        <Typography fontWeight="bold">This page could not be found.</Typography>
-        <Button
-          sx={{ mt: 2 }}
-          component={Link}
-          to={getDashboardPattern()}
-          type="button"
-          color="info"
-        >
-          Go Back to Home page
-        </Button>
-      </Box>
-    </Card>
-  </Grid>
+  <Card
+    sx={{
+      p: '50px',
+      textAlign: 'center',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '80vh',
+      m: 'auto'
+    }}
+  >
+    <Box>
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h3">ERROR PAGE</Typography>
+      <Typography>Uh Oh!! you're lost.</Typography>
+      <Typography fontWeight="bold">This page could not be found.</Typography>
+      <Button
+        sx={{ width: 'max-content', m: 'auto', mt: 2 }}
+        component={Link}
+        to={getDashboardPattern()}
+        type="button"
+        size="small"
+        color="info"
+      >
+        Go Back to Home page
+      </Button>
+    </Box>
+  </Card>
 );
 export default Error404;
