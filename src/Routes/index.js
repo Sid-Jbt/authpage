92788549a7fc0ee657c7da0fcf3e1d2a8f285efe
mridul-jarvisbitz <mridul.rathore@jarvisbitz.com>
2023-01-23 15:28:22 +0530

    routing is updated

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,23 @@
-import { useRoutes } from 'react-router';
+import Loadable from 'Elements/Loadable';
+import { lazy } from 'react';
+import { Navigate, useRoutes } from 'react-router';
 import AuthRoutes from './AuthRoutes';
 import DashboardRoutes from './MainRoutes';
+import { defaultPattern, errorPattern, loginPattern } from './routeConfig';
+
+const Error404 = Loadable(lazy(() => import('../Screens/Error404')));
 
 export default function RootRoutes() {
-  return useRoutes([AuthRoutes, DashboardRoutes]);
+  return useRoutes([
+    AuthRoutes,
+    DashboardRoutes,
+    {
+      path: errorPattern,
+      element: <Error404 />
+    },
+    {
+      path: loginPattern,
+      element: <Navigate to={defaultPattern} />
+    }
+  ]);
 }
