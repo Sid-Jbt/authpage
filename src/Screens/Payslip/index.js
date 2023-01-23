67263789d92563dbc2fa583bf35785e98f058664
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-01-23 18:12:46 +0530

    Get month with new select option on payslip screen

--- src/Screens/Payslip/index.js
+++ src/Screens/Payslip/index.js
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
-import { Card, Icon, MenuItem, Select, Grid, FormLabel, FormControl } from '@mui/material';
-import {
-  ImportExportRounded,
-  SearchRounded,
-  ClearRounded,
-  FilterListSharp
-} from '@mui/icons-material';
-import Typography from 'Elements/Typography';
+import { Card, Icon, Grid, FormLabel, FormControl } from '@mui/material';
+import { ImportExportRounded } from '@mui/icons-material';
 import Table from 'Elements/Tables/Table';
 import Button from 'Elements/Button';
-import Input from 'Elements/Input';
+import FilterLayout from 'Components/FilterLayout';
 import { Months, Years } from 'Helpers/Globle';
+import Select from 'Elements/Select';
 import payslipData from './data/payslipData';
 
 const Payslip = () => {
   const { columns: prCols, rows: prRows } = payslipData;
   const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
+  // const [year, setYear] = useState('');
 
-  const handleChangeMonth = (event) => {
-    setMonth(event.target.value);
+  const handleChangeMonth = (value) => {
+    setMonth(value.value);
   };
+  console.log('Month --> ', month, Years);
 
-  const handleChangeYear = (event) => {
-    setYear(event.target.value);
-  };
+  /* const handleChangeYear = (value) => {
+    setYear(value.value);
+  }; */
+  // console.log('Month & Year --> ', month, Years);
 
   return (
     <>
@@ -46,87 +43,21 @@ const Payslip = () => {
           boxShadow: ({ boxShadows: { md } }) => md
         }}
       >
-        <Grid container alignItems="center" spacing={2} p={2} pb={0} mb={2}>
-          <Grid container item xs={12}>
-            <Icon>
-              <FilterListSharp />
-            </Icon>
-            <Typography variant="h6">Filter</Typography>
-          </Grid>
-          <Grid item sm={12} md={4} lg={2}>
+        <FilterLayout>
+          <Grid item sm={12} md={4} lg={3}>
             <FormControl sx={{ width: '100%' }}>
               <FormLabel>Select Month</FormLabel>
-              <Select
-                id="selectMonth"
-                value={month}
-                onChange={handleChangeMonth}
-                displayEmpty
-                renderValue={month !== '' ? undefined : () => 'All'}
-              >
-                {Months.map((m, key) => (
-                  <MenuItem key={key} value={m}>
-                    {m}
-                  </MenuItem>
-                ))}
-              </Select>
+              <Select options={Months} onChange={(value) => handleChangeMonth(value)} />
             </FormControl>
           </Grid>
-          <Grid item sm={12} md={4} lg={2}>
+          <Grid item sm={12} md={4} lg={3}>
             <FormControl sx={{ width: '100%' }}>
               <FormLabel>Select Year</FormLabel>
-              <Select
-                id="selectMonth"
-                value={year}
-                onChange={handleChangeYear}
-                displayEmpty
-                renderValue={year !== '' ? undefined : () => 'All'}
-              >
-                {Years.map((y, key) => (
-                  <MenuItem key={key} value={y}>
-                    {y}
-                  </MenuItem>
-                ))}
-              </Select>
+              <Select />
             </FormControl>
           </Grid>
-          <Grid item sm={12} md={4} lg={2}>
-            <Input
-              placeholder="Search"
-              type="text"
-              label="Search"
-              size="small"
-              fullWidth
-              id="search"
-              name="search"
-              errorFalse
-            />
-          </Grid>
-          <Grid
-            item
-            sm={12}
-            md={8}
-            lg={4}
-            pt={4}
-            sx={({ breakpoints }) => ({
-              [breakpoints.down('lg' && 'md')]: {
-                marginBottom: 2
-              }
-            })}
-          >
-            <Button color="info" variant="contained" size="small" sx={{ marginRight: '10px' }}>
-              <Icon sx={{ mr: '2px' }}>
-                <SearchRounded />
-              </Icon>
-              Search
-            </Button>
-            <Button color="error" variant="contained" size="small">
-              <Icon sx={{ mr: '2px' }}>
-                <ClearRounded />
-              </Icon>
-              Clear
-            </Button>
-          </Grid>
-        </Grid>
+          {/* </Grid> */}
+        </FilterLayout>
         <Table columns={prCols} rows={prRows} />
       </Card>
     </>
