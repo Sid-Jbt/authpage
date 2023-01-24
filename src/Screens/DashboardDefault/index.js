313033a6d91072458879d9717b29e019819819d4
mridul-jarvisbitz <mridul.rathore@jarvisbitz.com>
2023-01-24 10:27:33 +0530

    Update index.js

--- src/Screens/DashboardDefault/index.js
+++ src/Screens/DashboardDefault/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import Box from 'Elements/Box';
-import Select from 'Elements/Select';
-import Dropzone from 'Elements/Dropzone';
-import Editor from 'Elements/Editor';
 
-const DashboardDefault = () => (
-  <Box py={3}>
-    Dashboard Default{' '}
-    <Select
-      options={[
-        { value: 'sickLeave', label: 'Sick Leave' },
-        { value: 'hospitalisation', label: 'Hospitalisation' },
-        { value: 'maternityLeave', label: 'Maternity Leave' }
-      ]}
-    />
-    <Dropzone />
-    <Editor />
-  </Box>
-);
+const DashboardDefault = () => <Box py={3}>Dashboard Default</Box>;
 
 export default DashboardDefault;
