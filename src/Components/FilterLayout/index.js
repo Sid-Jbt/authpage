dc8324895136e45e5799afcc2eef52bebfac317d
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-02-03 12:35:47 +0530

    Fixed: 9. Search is not coming 100% in the filter.

--- src/Components/FilterLayout/index.js
+++ src/Components/FilterLayout/index.js
@@ -37,7 +37,7 @@ const FilterLayout = ({ children }) => {
         <AccordionDetails sx={{ pt: 0, pb: 0 }}>
           <Grid container alignItems="flex-end" spacing={1}>
             {children}
-            <Grid item sm={12} md={4} lg={3}>
+            <Grid item xs={12} sm={12} md={4} lg={3}>
               <Input
                 placeholder="Search"
                 type="text"
