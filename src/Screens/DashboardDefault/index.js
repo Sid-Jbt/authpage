bcd9f55c9f3df4d6726472608d96c17c07990f5d
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-02-09 12:02:20 +0530

    Done: Add employee cards on dashboard for Admin only

--- src/Screens/DashboardDefault/index.js
+++ src/Screens/DashboardDefault/index.js
@@ -2,8 +2,12 @@ import React, { useMemo } from 'react';
 import Box from 'Elements/Box';
 import { Grid } from '@mui/material';
 import Calendar from 'Components/Calendar';
-import { Watch, WatchLater, WatchRounded } from '@mui/icons-material';
+import { PeopleRounded, Watch, WatchLater, WatchRounded } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+
 import LeaveCard from '../../Components/CardLayouts/LeaveCard';
+import { getEmployeeListPattern } from '../../Routes/routeConfig';
 
 const calendarEventsData = [
   {
@@ -58,6 +62,9 @@ const calendarEventsData = [
 
 const DashboardDefault = () => {
   let eventGuid = 0;
+  const { role } = useSelector((state) => state.route);
+  const navigate = useNavigate();
+
   const createEventId = () => String(eventGuid++);
   console.log('eventGuid --> ', eventGuid);
   const handleDateSelect = (selectInfo) => {
@@ -90,35 +97,92 @@ const DashboardDefault = () => {
     }
   };
 
+  const handleTotalEmployee = () => {
+    console.log('On redirect to Employee');
+    navigate(getEmployeeListPattern());
+  };
+
   return (
     <Box mb={3}>
       <Grid container spacing={3}>
         {/* <Grid container order={{ xs: 1, lg: 0 }} spacing={3} item xs={12} lg={7} xl={8}> */}
         <Grid container order={{ xs: 1, lg: 0 }} spacing={3} item xs={12} lg={12} xl={12}>
-          <Grid item xs={12} md={6} lg={3}>
-            <LeaveCard
-              title="Today"
-              count="07:15:34"
-              icon={{ color: 'success', component: <Watch /> }}
-              isPercentage={false}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <LeaveCard
-              title="This week"
-              count="45 hours "
-              icon={{ color: 'secondary', component: <WatchRounded /> }}
-              isPercentage={false}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <LeaveCard
-              title="This month"
-              count="160 hours"
-              icon={{ color: 'info', component: <WatchLater /> }}
-              isPercentage={false}
-            />
-          </Grid>
+          {role === 'admin' ? (
+            <>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                lg={3}
+                onClick={handleTotalEmployee}
+                sx={{ cursor: 'pointer' }}
+              >
+                <LeaveCard
+                  title="Total Employee"
+                  count="10"
+                  icon={{ color: 'info', component: <PeopleRounded /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                lg={3}
+                onClick={handleTotalEmployee}
+                sx={{ cursor: 'pointer' }}
+              >
+                <LeaveCard
+                  title="Today Present"
+                  count="9"
+                  icon={{ color: 'success', component: <PeopleRounded /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                lg={3}
+                onClick={handleTotalEmployee}
+                sx={{ cursor: 'pointer' }}
+              >
+                <LeaveCard
+                  title="Today Absent"
+                  count="1"
+                  icon={{ color: 'error', component: <PeopleRounded /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+            </>
+          ) : (
+            <>
+              <Grid item xs={12} md={6} lg={3}>
+                <LeaveCard
+                  title="Today"
+                  count="07:15:34"
+                  icon={{ color: 'success', component: <Watch /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} lg={3}>
+                <LeaveCard
+                  title="This week"
+                  count="45 hours "
+                  icon={{ color: 'secondary', component: <WatchRounded /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} lg={3}>
+                <LeaveCard
+                  title="This month"
+                  count="160 hours"
+                  icon={{ color: 'info', component: <WatchLater /> }}
+                  isPercentage={false}
+                />
+              </Grid>
+            </>
+          )}
           <Grid item xs={12}>
             {useMemo(
               () => (
