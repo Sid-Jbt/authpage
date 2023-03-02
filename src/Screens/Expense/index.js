79d29c80fa24e0164f4add6c9dfeba2558d7c0c6
Hetali JBT <hetali.shah@jarvisBitz.com>
2023-03-02 15:45:01 +0530

    Server Testing

--- src/Screens/Expense/index.js
+++ src/Screens/Expense/index.js
@@ -42,6 +42,7 @@ const Expense = () => {
   const [isEdit, setIsEdit] = useState(false);
   const [counts, setCounts] = useState(null);
   const [loader, setLoader] = useState(false);
+  console.log('loder');
 
   const [allExpenseList, setAllExpenseList] = useState([]);
   const [expenseListCount, setExpenseListCount] = useState(0);
