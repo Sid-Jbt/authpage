5b0ba002925a1f38094ba7f30b4cb80888b1d176
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-03-24 17:55:37 +0530

    Update working hours in gloabl

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -63,54 +63,10 @@ export const Gender = [
   { value: 'female', label: 'Female' }
 ];
 
-export const Days = [
-  { value: 1, label: 1 },
-  { value: 2, label: 2 },
-  { value: 3, label: 3 },
-  { value: 4, label: 4 },
-  { value: 5, label: 5 },
-  { value: 6, label: 6 },
-  { value: 7, label: 7 },
-  { value: 8, label: 8 },
-  { value: 9, label: 9 },
-  { value: 10, label: 10 },
-  { value: 11, label: 11 },
-  { value: 12, label: 12 },
-  { value: 13, label: 13 },
-  { value: 14, label: 14 },
-  { value: 15, label: 15 },
-  { value: 16, label: 16 },
-  { value: 17, label: 17 },
-  { value: 18, label: 18 },
-  { value: 19, label: 19 },
-  { value: 20, label: 20 },
-  { value: 21, label: 21 },
-  { value: 22, label: 22 },
-  { value: 23, label: 23 },
-  { value: 24, label: 24 },
-  { value: 25, label: 25 },
-  { value: 26, label: 26 },
-  { value: 27, label: 27 },
-  { value: 28, label: 28 },
-  { value: 29, label: 29 },
-  { value: 30, label: 30 },
-  { value: 31, label: 31 }
-];
-
-export const WorkingHours = [
-  { value: '1', label: '01:00' },
-  { value: '2', label: '02:00' },
-  { value: '3', label: '03:00' },
-  { value: '4', label: '04:00' },
-  { value: '5', label: '05:00' },
-  { value: '6', label: '06:00' },
-  { value: '7', label: '07:00' },
-  { value: '8', label: '08:00' },
-  { value: '9', label: '09:00' },
-  { value: '10', label: '10:00' },
-  { value: '11', label: '11:00' },
-  { value: '12', label: '12:00' }
-];
+export const WorkingHours = [];
+for (let i = 1; i <= 12; i++) {
+  WorkingHours.push({ value: `${i.toString()}`, label: `${i.toString()}:00` });
+}
 
 export const Priority = [
   { value: 'high', label: 'High' },
