3ec00f18760852dcfa0786c61242a2aa42813019
hiren-jarvisbitz <hiren.gevariya@jarvisbitz.com>
2023-04-04 13:48:51 +0530

    Updated scroll

--- src/Theme/compStyleOverride.js
+++ src/Theme/compStyleOverride.js
@@ -27,6 +27,17 @@ export default function componentStyleOverrides(theme) {
         html: {
           scrollBehavior: 'smooth'
         },
+        '*::-webkit-scrollbar': {
+          width: '0.2em'
+        },
+        '*::-webkit-scrollbar-track': {
+          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          backgroundColor: `${dark.main}`,
+          outline: `1px solid ${dark.main}`,
+          borderRadius: '10px'
+        },
         '*, *::before, *::after': {
           margin: 0,
           padding: 0
