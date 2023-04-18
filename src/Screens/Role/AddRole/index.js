e1bf54c22262249d2bd8130a240767552dcee752
Hetali JBT <hetali.shah@jarvisBitz.com>
2023-04-18 18:22:54 +0530

    Done: Remove unused box

--- src/Screens/Role/AddRole/index.js
+++ src/Screens/Role/AddRole/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Box from 'Elements/Box';
 import Typography from 'Elements/Typography';
 import { Card, FormControlLabel, Grid, Switch } from '@mui/material';
 import Input from 'Elements/Input';
@@ -31,156 +30,154 @@ const AddRole = () => {
   };
 
   return (
-    <Box>
-      <Card
+    <Card
+      sx={{
+        py: 2,
+        px: 2,
+        boxShadow: ({ boxShadows: { sm } }) => sm
+      }}
+    >
+      <Grid
+        container
+        spacing={2}
         sx={{
-          py: 2,
-          px: 2,
-          boxShadow: ({ boxShadows: { sm } }) => sm
+          display: 'flex',
+          flexDirection: 'column'
         }}
       >
-        <Grid
-          container
-          spacing={2}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column'
-          }}
-        >
-          <Grid item sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button type="submit" color="info" variant="contained" size="small">
-              Save
-            </Button>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Input
-              type="text"
-              placeholder="Role Name"
-              size="large"
-              fullWidth
-              id="roleName"
-              name="roleName"
-              label="Role Name"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            {Object.keys(modules).map((key) => (
-              <Grid container sx={{ padding: 1 }}>
-                <Grid item xs={12} md={2}>
-                  <Typography
-                    variant="subtitle2"
-                    color="text"
-                    fontWeight="bold"
-                    textTransform="capitalize"
-                    sx={{ paddingRight: 2 }}
-                  >
-                    {key}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={10}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row'
-                  }}
-                >
-                  <FormControlLabel
-                    sx={{ m: 0, fontSize: '14px' }}
-                    value={modules[key].read === 1}
-                    control={
-                      <Switch
-                        checked={modules[key].read === 1}
-                        color="primary"
-                        name="read"
-                        onChange={() => onChangePermission(key, 'read')}
-                      />
-                    }
-                    label={
-                      <Typography
-                        variant="button"
-                        fontWeight="regular"
-                        sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
-                      >
-                        Read
-                      </Typography>
-                    }
-                    labelPlacement="end"
-                  />
-                  <FormControlLabel
-                    sx={{ m: 0, fontSize: '14px' }}
-                    value={modules[key].write === 1}
-                    control={
-                      <Switch
-                        checked={modules[key].write === 1}
-                        color="primary"
-                        name="write"
-                        onChange={() => onChangePermission(key, 'write')}
-                      />
-                    }
-                    label={
-                      <Typography
-                        variant="button"
-                        fontWeight="regular"
-                        sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
-                      >
-                        Write
-                      </Typography>
-                    }
-                    labelPlacement="end"
-                  />
-                  <FormControlLabel
-                    sx={{ m: 0, fontSize: '14px' }}
-                    value={modules[key].view === 1}
-                    control={
-                      <Switch
-                        checked={modules[key].view === 1}
-                        color="primary"
-                        name="view"
-                        onChange={() => onChangePermission(key, 'view')}
-                      />
-                    }
-                    label={
-                      <Typography
-                        variant="button"
-                        fontWeight="regular"
-                        sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
-                      >
-                        View
-                      </Typography>
-                    }
-                    labelPlacement="end"
-                  />
-                  <FormControlLabel
-                    sx={{ m: 0, fontSize: '14px' }}
-                    value={modules[key].delete === 1}
-                    control={
-                      <Switch
-                        checked={modules[key].delete === 1}
-                        color="primary"
-                        name="delete"
-                        onChange={() => onChangePermission(key, 'delete')}
-                      />
-                    }
-                    label={
-                      <Typography
-                        variant="button"
-                        fontWeight="regular"
-                        sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
-                      >
-                        Delete
-                      </Typography>
-                    }
-                    labelPlacement="end"
-                  />
-                </Grid>
-              </Grid>
-            ))}
-          </Grid>
+        <Grid item sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button type="submit" color="info" variant="contained" size="small">
+            Save
+          </Button>
         </Grid>
-      </Card>
-    </Box>
+        <Grid item xs={12} md={6} lg={4}>
+          <Input
+            type="text"
+            placeholder="Role Name"
+            size="large"
+            fullWidth
+            id="roleName"
+            name="roleName"
+            label="Role Name"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          {Object.keys(modules).map((key) => (
+            <Grid container sx={{ padding: 1 }}>
+              <Grid item xs={12} md={2}>
+                <Typography
+                  variant="subtitle2"
+                  color="text"
+                  fontWeight="bold"
+                  textTransform="capitalize"
+                  sx={{ paddingRight: 2 }}
+                >
+                  {key}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={10}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'row'
+                }}
+              >
+                <FormControlLabel
+                  sx={{ m: 0, fontSize: '14px' }}
+                  value={modules[key].read === 1}
+                  control={
+                    <Switch
+                      checked={modules[key].read === 1}
+                      color="primary"
+                      name="read"
+                      onChange={() => onChangePermission(key, 'read')}
+                    />
+                  }
+                  label={
+                    <Typography
+                      variant="button"
+                      fontWeight="regular"
+                      sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
+                    >
+                      Read
+                    </Typography>
+                  }
+                  labelPlacement="end"
+                />
+                <FormControlLabel
+                  sx={{ m: 0, fontSize: '14px' }}
+                  value={modules[key].write === 1}
+                  control={
+                    <Switch
+                      checked={modules[key].write === 1}
+                      color="primary"
+                      name="write"
+                      onChange={() => onChangePermission(key, 'write')}
+                    />
+                  }
+                  label={
+                    <Typography
+                      variant="button"
+                      fontWeight="regular"
+                      sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
+                    >
+                      Write
+                    </Typography>
+                  }
+                  labelPlacement="end"
+                />
+                <FormControlLabel
+                  sx={{ m: 0, fontSize: '14px' }}
+                  value={modules[key].view === 1}
+                  control={
+                    <Switch
+                      checked={modules[key].view === 1}
+                      color="primary"
+                      name="view"
+                      onChange={() => onChangePermission(key, 'view')}
+                    />
+                  }
+                  label={
+                    <Typography
+                      variant="button"
+                      fontWeight="regular"
+                      sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
+                    >
+                      View
+                    </Typography>
+                  }
+                  labelPlacement="end"
+                />
+                <FormControlLabel
+                  sx={{ m: 0, fontSize: '14px' }}
+                  value={modules[key].delete === 1}
+                  control={
+                    <Switch
+                      checked={modules[key].delete === 1}
+                      color="primary"
+                      name="delete"
+                      onChange={() => onChangePermission(key, 'delete')}
+                    />
+                  }
+                  label={
+                    <Typography
+                      variant="button"
+                      fontWeight="regular"
+                      sx={{ cursor: 'pointer', userSelect: 'none', paddingRight: 2 }}
+                    >
+                      Delete
+                    </Typography>
+                  }
+                  labelPlacement="end"
+                />
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Card>
   );
 };
 export default AddRole;
